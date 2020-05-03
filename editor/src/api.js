38e0fa10c4aa8e64697f306921bbed68048743f3
mikhael <mikhael@milikhin.name>
2020-05-03 20:09:51 +0700

    [editor] add jsDoc to API methods

--- editor/src/api.js
+++ editor/src/api.js
@@ -12,28 +12,55 @@ class Api {
     }
   }
 
-  // #region AP
+  // #region API
 
+  /**
+   * 'beautify' command handler: intended to auto format file content
+   * @returns undefined
+   */
   _apiOnBeautify () {
     this._editor.beautify()
   }
 
+  /**
+   * 'navigateLeft' command handler: intended to move cursor left
+   * @returns undefined
+   */
   _apiOnNavigateLeft () {
     this._editor.navigateLeft()
   }
 
+  /**
+   * 'navigateRight' command handler: intended to move cursor right
+   * @returns undefined
+   */
   _apiOnNavigateRight () {
     this._editor.navigateRight()
   }
 
-  _apiOnNavigateUp () {
-    this._editor.navigateUp()
-  }
-
+  /**
+   * 'navigateDown' command handler: intended to move cursor down
+   * @returns undefined
+   */
   _apiOnNavigateDown () {
     this._editor.navigateDown()
   }
 
+  /**
+   * 'navigateUp' command handler: intended to move cursor up
+   * @returns undefined
+   */
+  _apiOnNavigateUp () {
+    this._editor.navigateUp()
+  }
+
+  /**
+   * 'loadFile' command handler: intended to load given content to the editor
+   * @param {string} filePath - /path/to/file - used as file ID
+   * @param {string} content - file content
+   * @param {boolean} [readOnly=false] - read only flag
+   * @returns undefined
+   */
   _apiOnLoadFile ({ filePath, content = '', readOnly = false }) {
     if (!filePath) {
       throw new InvalidArgError(`${filePath} is required to load file into editor`)
@@ -42,10 +69,19 @@ class Api {
     this._editor.loadFile(filePath, content, readOnly)
   }
 
+  /**
+   * 'redo' command handler: intended to redo latest changes
+   * @returns undefined
+   */
   _apiOnRedo () {
     this._editor.redo()
   }
 
+  /**
+   * 'requestFileSave' command handler: intended to request and save file content
+   * @param {string} filePath - /path/to/file - used as file ID
+   * @returns undefined
+   */
   _apiOnRequestSaveFile ({ filePath }) {
     const value = this._editor.getContent(filePath)
     this._sendApiMessage('saveFile', {
@@ -55,15 +91,23 @@ class Api {
     })
   }
 
+  /**
+   * 'toggleReadOnly' command handler: intended to toggle readOnly mode
+   * @returns undefined
+   */
   _apiOnToggleReadOnly () {
     this._editor.toggleReadOnly()
   }
 
+  /**
+   * 'undo' command handler: intended to undo latest changes
+   * @returns undefined
+   */
   _apiOnUndo () {
     this._editor.undo()
   }
 
-  // #endregion
+  // #endregion API
 
   _handleStateChanged = ({ hasUndo, hasRedo, filePath, isReadOnly }) => {
     this._sendApiMessage('stateChanged', {
