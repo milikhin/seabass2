b9681c12c3d589b4aa10af4249f3f2fe404de0c5
mikhael <mikhael@milikhin.name>
2020-06-14 01:21:53 +0700

    add unit test for fs_model.list_dir

---
+++ generic/py-backend/fs_model_test.py
@@ -0,0 +1,29 @@
+"""Unit tests for fs_model.py"""
+
+from os.path import join
+from fs_model import list_dir
+
+def test_same_dir_files(fs): # pylint: disable=invalid-name
+    """Should sort files in the same dir alphabetically"""
+    home = '/home/user'
+    file0 = join(home, 'foo')
+    file1 = join(home, 'bar')
+    fs.create_file(file0)
+    fs.create_file(file1)
+
+    result = list_dir(home)
+    assert len(result) == 2
+    assert result[0] == {
+        "name": "bar",
+        "path": file1,
+        "isDir": False,
+        "isFile": True,
+        "level": 0
+    }
+    assert result[0] == {
+        "name": "foo",
+        "path": file0,
+        "isDir": False,
+        "isFile": True,
+        "level": 0
+    }
