a6728b5ade22f850eaccbc4ec3800af50688e6bd
Mikhael Milikhin <mikhael@milikhin.name>
2020-06-14 21:52:57 +0700

    add unit test for sorting files in python

--- generic/py-backend/tests/fs_model_test.py
+++ generic/py-backend/tests/fs_model_test.py
@@ -27,3 +27,37 @@ def test_same_dir_files(fs): # pylint: disable=invalid-name
         "isFile": True,
         "level": 0
     }
+
+def test_diff_dir_files(fs): # pylint: disable=invalid-name
+    """Should sort nested children before next sibling"""
+    home = '/home/user'
+    file0 = join(home, 'abc')
+    file1 = join(home, 'abc/foo')
+    file2 = join(home, 'bar')
+    fs.create_dir(file0)
+    fs.create_file(file1)
+    fs.create_file(file2)
+
+    result = list_dir(home, [file0])
+    assert len(result) == 3
+    assert result[0] == {
+        "name": "abc",
+        "path": file0,
+        "isDir": True,
+        "isFile": False,
+        "level": 0
+    }
+    assert result[1] == {
+        "name": "foo",
+        "path": file1,
+        "isDir": False,
+        "isFile": True,
+        "level": 1
+    }
+    assert result[2] == {
+        "name": "bar",
+        "path": file2,
+        "isDir": False,
+        "isFile": True,
+        "level": 0
+    }
