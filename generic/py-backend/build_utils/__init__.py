fa1c3f4088fc037bc93a2ae35d7cd26176a6e6dc
Mikhael Milikhin <mikhael@milikhin.name>
2020-06-24 00:07:28 +0700

    ubports-seabass@0.5.1 hotfix (#66)
    * [ubports-seabass] remove write_path from apparmor profile, fix issues with container deletion on setup errors, fix issues with removing desktop files from build container

--- generic/py-backend/build_utils/__init__.py
+++ generic/py-backend/build_utils/__init__.py
@@ -1,7 +1,8 @@
 """
-  build utils:
+  [UBports only!] build utils:
 
-  * build() - run build inside a Libertine container
+  * build(config_file) - run build inside a Libertine container
+  * test_container_exists() - check whether Build container exists or not
 """
 
 __all__ = ["build", "test_container_exists"]
