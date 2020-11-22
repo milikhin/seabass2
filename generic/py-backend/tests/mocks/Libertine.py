c3e9075e8e0876a4e7e3545e7903a1a12174725a
Mikhail Milikhin <mikhael@milikhin.name>
2020-11-22 15:11:20 +0200

    fix python tests

--- generic/py-backend/tests/mocks/Libertine.py
+++ generic/py-backend/tests/mocks/Libertine.py
@@ -4,6 +4,9 @@ class LibertineContainer:
     def __init__(self, container_id, containers_config=None, service=None):
         pass
 
+    def configure_add_archive(self, url, key):
+        pass
+
     def update_libertine_container(self):
         pass
 
