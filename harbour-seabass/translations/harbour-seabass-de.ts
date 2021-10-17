dd81b47617e1a5e73632c526e85a8813558b0dba
Mikhail Milikhin <mikhail.milikhin@rubius.com>
2021-10-17 13:31:50 +0200

    [harbour-seabass] remove UI scaling fix for Jolla Phone; update release notes

--- harbour-seabass/translations/harbour-seabass-de.ts
+++ harbour-seabass/translations/harbour-seabass-de.ts
@@ -11,28 +11,28 @@
 <context>
     <name>Editor</name>
     <message>
-        <source>Save</source>
-        <translation>Speichern</translation>
-    </message>
-    <message>
-        <source>Open file...</source>
-        <translation>Datei öffnen...</translation>
-    </message>
-    <message>
-        <source>Saving...</source>
-        <translation>Speichern...</translation>
-    </message>
-    <message>
-        <source>About</source>
-        <translation>Über</translation>
-    </message>
-    <message>
         <source>Unable to read file. Please ensure that you have read access to the %1</source>
-        <translation>Datei kann nicht gelesen werden. Bitte stelle sicher, dass Lesezugriff besteht für %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Unable to write the file. Please ensure that you have write access to %1</source>
-        <translation>Datei kann nicht geschrieben werden. Bitte stelle sicher, dass Schreibzugriff besteht für %1</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open file...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saving...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
