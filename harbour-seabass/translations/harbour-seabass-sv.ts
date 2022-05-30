38acb025f9bb51a12573d73684cbca057b5f275d
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-05-30 12:45:48 +0200

    Create harbour-seabass-sv.ts
    Add Swedish translation and fix some translations errors.

---
+++ harbour-seabass/translations/harbour-seabass-sv.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="su">
+<context>
+    <name>About</name>
+    <message>
+        <source>Seabass v%1</source>
+        <translation>Seabass v%1</translation>
+    </message>
+</context>
+<context>
+    <name>Editor</name>
+    <message>
+        <source>Unable to read file. Please ensure that you have read access to the %1</source>
+        <translation>Kunde inte läsa filen. Tillse att du har läsbehörighet på %1</translation>
+    </message>
+    <message>
+        <source>Unable to write the file. Please ensure that you have write access to %1</source>
+        <translation>Kunde inte skriva filen. Tillse att du skrivbehörighet på %1</translation>
+    </message>
+    <message>
+        <source>Open file...</source>
+        <translation>Öppna fil...</translation>
+    </message>
+    <message>
+        <source>Saving...</source>
+        <translation>Sparar...</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Hide toolbar</source>
+        <translation>Dölj verktygsfält</translation>
+    </message>
+    <message>
+        <source>Show toolbar</source>
+        <translation>Visa verktygsfält</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <source>Error occured</source>
+        <translation>Fel inträffade</translation>
+    </message>
+</context>
+<context>
+    <name>SaveDialog</name>
+    <message>
+        <source>Discard changes?</source>
+        <translation>Vill du kassera ändringar?</translation>
+    </message>
+    <message>
+        <source>Unsaved changes at %1 will be lost if you open another file. Continue?</source>
+        <translation>Osparade ändringar på %1 kommer att förloras om du öppnar en annan fil. Fortsätta?</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-seabass</name>
+    <message>
+        <source>Welcome</source>
+        <translation>Välkommen</translation>
+    </message>
+</context>
+<context>
+    <name>Toolbar</name>
+    <message>
+        <source>Read only</source>
+        <translation>Skrivskydd</translation>
+    </message>
+</context>
+</TS>
